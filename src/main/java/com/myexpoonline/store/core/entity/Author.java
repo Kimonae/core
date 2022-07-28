151f59c07382b58a2a3c7a496750f4f8aec42160
Kimonae <shinokasu@gmail.com>
2022-07-28 16:34:22 +0200

    works duo

---
+++ src/main/java/com/myexpoonline/store/core/entity/Author.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myexpoonline.store.core.entity;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Kimonae
+ */
+public class Author implements Serializable {
+    
+    private String name;
+
+    
+public Author (String name)
+{
+this.name =name;
+}
+
+public Author ()
+{
+this.name =name;
+}
+
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+}
---
+++ src/main/java/com/myexpoonline/store/core/entity/Work.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myexpoonline.store.core.entity;
+
+/**
+ *
+ * @author Kimonae
+ */
+public class Work {
+
+    private String title;
+    private int year;
+    private String description;
+    private String author;
+    private String style;
+    
+    
+  public Work (String title, int year, String description, String author, String style)
+{
+this.title =title;
+this.year =year;
+this.description =description;
+this.author =author;
+this.style =style;
+
+}  
+
+public Work ()
+{
+
+}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+
+
+}
+
